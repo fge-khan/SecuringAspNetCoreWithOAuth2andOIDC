7a2b57fe1746f39c306eb53d047958597f469229
Franck Engoulevent <franckengoulevent@gmail.com>
2024-09-08 21:13:11 +0200

    Using custom attribute for authorizatioin

--- Starter files/ImageGallery.API/Controllers/ImagesController.cs
+++ Starter files/ImageGallery.API/Controllers/ImagesController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ImageGallery.API.Authorization;
 using ImageGallery.API.Services;
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authorization;
@@ -49,7 +50,8 @@ namespace ImageGallery.API.Controllers
         }
 
         [HttpGet("{id}", Name = "GetImage")]
-        [Authorize("MustOwnImage")]
+        //[Authorize("MustOwnImage")]
+        [MustOwnImage]
         public async Task<ActionResult<Image>> GetImage(Guid id)
         {
             var imageFromRepo = await _galleryRepository.GetImageAsync(id);
